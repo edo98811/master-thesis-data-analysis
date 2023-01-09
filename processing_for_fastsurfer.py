733a72bd2ad77c090e131df7df1e9e97aaa7abea
edo98811 <edoardofilippi77@gmail.com>
2023-01-09 11:33:16 +0000

    script aggiustati

--- processing_for_fastsurfer.py
+++ processing_for_fastsurfer.py
@@ -5,7 +5,7 @@ def main():
     basepath = "/media/neuropsycad/disk12t/VascoDiogo/OASIS/FS7/"
 
     # save all the useful images
-    images_list = dm.list_files(basepath, "001")
+    images_list = dm.list_files(basepath, "001.mgz")
     dm.save_dict(images_list,"list_original_images.json")
 
 
