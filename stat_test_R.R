5981fd769e070992086f8c469a2798deffecf943
edo98811 <edoardofilippi77@gmail.com>
2023-02-09 11:39:35 +0000

    modificati test in r

--- stat_test_R.R
+++ stat_test_R.R
@@ -22,12 +22,12 @@ save_csv <- function(base_path, list_line) {
 get_column <- function(column_to_compare, df1, df2) {
 
     if (is.numeric(column_to_compare)) {
-        a <- df1[,column_to_compare]
-        b <- df2[,column_to_compare]
+        a = df1[,column_to_compare]
+        b = df2[,column_to_compare]
     }
     if (is.character(column_to_compare)) {
-        a <- df1[,column_to_compare]
-        b <- df2[,column_to_compare]
+        a = df1[,column_to_compare]
+        b = df2[,column_to_compare]
     }
 
 return(list(a=a, b=b))
@@ -37,63 +37,69 @@ mann_whitney <- function(base_path, filename1, filename2, column_to_compare) {
     library(dplyr)
     library(stats)
     
-    df1 <- read.csv(paste(base_path, filename1, sep=""))
-    df2 <- read.csv(paste(base_path, filename2, sep=""))
+    df1 = read.csv(paste(base_path, filename1, sep=""))
+    df2 = read.csv(paste(base_path, filename2, sep=""))
     
-    df1 <- df1[df1$subjects %in% df2$subjects, ]
+    df1 = df1[df1$subjects %in% df2$subjects, ]
 
-    result <- get_column(column_to_compare, df1, df2)
-    a <- result$a
-    b <- result$b
+    # result = get_column(column_to_compare, df1, df2)
+    # a = result$a
+    # b = result$b
+    a = df1[column_to_compare]
+    b = df2[column_to_compare]
+    p_value = NA
     
     if (any(is.na(c(a,b)))) {
         return(c("result could not be computed", "NaN", "NaN"))
     }
     
-    t_stat <- wilcox.test(a, b)$statistic
-    p_value <- wilcox.test(a, b)$p.value
+    t_stat = wilcox.test(a, b)$statistic
+    p_value = wilcox.test(a, b)$p.value
 
     if (p_value == NA){
-        result <- paste("result could not be computed")
-        outcome <- -1    
+        result = paste("result could not be computed")
+        outcome = -1    
     } else if (p_value > 0.05) {
-        result <- paste("p-value:", p_value, "- null hypothesis cannot be rejected, the datasets have the same distribution")
-        outcome <- 0
+        result = paste("p-value:", p_value, "- null hypothesis cannot be rejected, the datasets have the same distribution")
+        outcome = 0
     } else {
-        result <- paste("p-value:", p_value, "- null hypothesis rejected, the datasets have a different distribution")
-        outcome <- 1
+        result = paste("p-value:", p_value, "- null hypothesis rejected, the datasets have a different distribution")
+        outcome = 1
     }
     
   return(list(result=result, p_value=p_value, outcome=outcome))
 }
 
 t_test <- function(base_path, filename1, filename2, column_to_compare) {
-    df1 <- read.csv(paste(base_path, filename1, sep=""))
-    df2 <- read.csv(paste(base_path, filename2, sep=""))
+    df1 = read.csv(paste(base_path, filename1, sep=""))
+    df2 = read.csv(paste(base_path, filename2, sep=""))
     
-    df1 <- df1[df1$subjects %in% df2$subjects,]
+    df1 = df1[df1$subjects %in% df2$subjects,]
 
-    result <- get_column(column_to_compare, df1, df2)
-    a <- result$a
-    b <- result$b
+    # result = get_column(column_to_compare, df1, df2)
+    # a = result$a
+    # b = result$b
+    a = df1[column_to_compare]
+    b = df2[column_to_compare]
+    p_value = NA
     
     if (any(is.na(c(a, b)))) {
         print("could not compute")
         return(list(result="result could not be computed", p_value="NaN", outcome="NaN"))
     }
     
-    t.test <- t.test(a, b)
-    p_value <- t.test$p.value
+    t.test = t.test(a, b)
+    p_value = t.test$p.value
 
     if (p_value == NA){
-        result <- paste("result could not be computed")
-        outcome <- -1    
+        result = paste("result could not be computed")
+        outcome = -1    
     } else if (p_value > 0.05) {
-        result <- paste("p-value:", p_value, "- null hypothesis cannot be rejected, means are statistically equal")
-        outcome <- 0
+        result = paste("p-value:", p_value, "- null hypothesis cannot be rejected, means are statistically equal")
+        outcome = 0
     } else {
-        result <- paste("p-value:", p_value, "- null hypothesis rejected, means are not statistically equal")
-        outcome <- 1
+        result = paste("p-value:", p_value, "- null hypothesis rejected, means are not statistically equal")
+        outcome = 1
   }
   
   return(list(result=result, p_value=p_value, outcome=outcome))
@@ -102,14 +108,14 @@ t_test <- function(base_path, filename1, filename2, column_to_compare) {
 
 
 stat_test <- function(base_path, filename1, filename2, column_to_compare, r_all) {
-    r1 <- mann_whitney(base_path, filename1, filename2, column_to_compare)
-    r2 <- t_test(base_path, filename1, filename2, column_to_compare)
+    r1 = mann_whitney(base_path, filename1, filename2, column_to_compare)
+    r2 = t_test(base_path, filename1, filename2, column_to_compare)
     
     if (is.numeric(column_to_compare)) {
-        df_example <- read.csv(paste0(base_path, filename2))
-        column_to_compare_name <- names(df_example)[column_to_compare + 1] # R is 1-indexed, python is 0-indexed
+        df_example = read.csv(paste0(base_path, filename2))
+        column_to_compare_name = names(df_example)[column_to_compare + 1] # R is 1-indexed, python is 0-indexed
         
-        r_all[[length(r_all) + 1]] <- list(name = paste0(filename1, " ", column_to_compare_name), 
+        r_all[[length(r_all) + 1]] = list(name = paste0(filename1, " ", column_to_compare_name), 
                                         mann_whitney = list(result = r1[1], 
                                                             p_value = r1[2], 
                                                             outcome = r1[3]), 
@@ -120,7 +126,7 @@ stat_test <- function(base_path, filename1, filename2, column_to_compare, r_all)
     }
     
     if (is.character(column_to_compare)) {
-        r_all[[length(r_all) + 1]] <- list(name = paste0(filename1, " ", column_to_compare), 
+        r_all[[length(r_all) + 1]] = list(name = paste0(filename1, " ", column_to_compare), 
                                         mann_whitney = list(result = r1[1], 
                                                             p_value = r1[2], 
                                                             outcome = r1[3]), 
