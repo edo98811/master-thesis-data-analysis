01a9f38a03cb9b302186e32ae664918a2dc49e9b
edo98811 <edoardofilippi77@gmail.com>
2023-03-02 16:23:46 +0000

    aggiunto il comparison plot (un pochino) e risolti altri errori

--- main.py
+++ main.py
@@ -7,23 +7,27 @@ BASE_PATH = "/media/neuropsycad/disk12t/EdoardoFilippiMasterThesis/"
 DATA_FOLDER = "test_data/"
 
 
-
 def main():
-    table = ft.Table(pd.read_csv(BASE_PATH + "/text_and_csv_files/OASIS_table.csv"), OASIS_PATH, BASE_PATH + "/text_and_csv_files")
+    table = ft.Table(pd.read_csv(BASE_PATH + "/text_and_csv_files/OASIS_table.csv"), OASIS_PATH,
+                     BASE_PATH + "/text_and_csv_files")
 
     aseg_free = pd.read_csv(BASE_PATH + "Stats_FreeSurfer/aseg.csv")
     aparcL_free = pd.read_csv(BASE_PATH + "Stats_FreeSurfer/aparcDKT_right.csv")
     aparcR_free = pd.read_csv(BASE_PATH + "Stats_FreeSurfer/aparcDKT_left.csv")
 
-    stats_fast_healthy = ft.Stats(table, "healthy_FAST"," main_condition!='Cognitively normal'", d_folder=DATA_FOLDER, b_path=BASE_PATH)
-    stats_fast_MC = ft.Stats(table, "MC_FAST", "main_condition!='Cognitively normal'", d_folder=DATA_FOLDER, b_path=BASE_PATH)
+    stats_fast_healthy = ft.Stats("healthy_FAST", BASE_PATH, table, " main_condition!='Cognitively normal'",
+                                  d_folder=DATA_FOLDER)
+    stats_fast_MC = ft.Stats("MC_FAST", BASE_PATH, table, "main_condition!='Cognitively normal'", d_folder=DATA_FOLDER)
 
-    stats_free_healthy = ft.Stats(table, "healthy_FREE", "main_condition!='Cognitively normal'" , BASE_PATH, DATA_FOLDER)
-    stats_free_MC = ft.Stats(table, "MC_FREE", "main_condition!='Cognitively normal'", BASE_PATH, DATA_FOLDER, aseg=aseg_free,
+    stats_free_healthy = ft.Stats("healthy_FREE", BASE_PATH, table, "main_condition!='Cognitively normal'", BASE_PATH,
+                                  DATA_FOLDER, aseg=aseg_free,
+                                  aparcRight=aparcR_free, aparcLeft=aparcL_free)
+    stats_free_MC = ft.Stats("MC_FREE", BASE_PATH, table, "main_condition!='Cognitively normal'", BASE_PATH,
+                             DATA_FOLDER, aseg=aseg_free,
                              aparcRight=aparcR_free, aparcLeft=aparcL_free)
 
-    comp1 = ft.Comparisons(stats_free_MC, stats_fast_MC, "MC_oasis", 0.05, DATA_FOLDER, BASE_PATH)
-    comp2 = ft.Comparisons(stats_free_healthy, stats_fast_healthy, "healthy_oasis", DATA_FOLDER, 0.05, BASE_PATH)
+    comp1 = ft.Comparisons("MC_oasis", BASE_PATH, stats_free_MC, stats_fast_MC)
+    comp2 = ft.Comparisons("healthy_oasis", BASE_PATH, stats_free_healthy, stats_fast_healthy)
     comp1.bonferroni_correction()
     comp2.bonferroni_correction()
 
