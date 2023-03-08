2a1b551113d3eec7c4fd70036c92f2e303b723db
edo98811 <edoardofilippi77@gmail.com>
2023-03-08 12:23:43 +0000

    debugging testing

--- FastsurferTesting.py
+++ FastsurferTesting.py
@@ -365,6 +365,7 @@ class Stats:
         #
 
         print(len(self.subj_list))
+        print(len(self.df_stats_aseg["ID"].tolist()))
         self.subj_list = [v for v in self.subj_list if v in self.df_stats_aseg["ID"].tolist()]
         temp = set(self.delete_sub(self.subj_list))
         self.df_subj = self.df_subj[self.df_subj["ID"].isin(temp)]
