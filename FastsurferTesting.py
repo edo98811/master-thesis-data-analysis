9acd328638ae8c5e3ed18230e0f391d0040c2c51
edo98811 <edoardofilippi77@gmail.com>
2023-03-08 11:41:12 +0000

    debugging testing

--- FastsurferTesting.py
+++ FastsurferTesting.py
@@ -717,7 +717,8 @@ class Stats:
         paths_found = []
         if alg == "fast":
             for s in self.subj_list:
-                s_path = os.path.dirname(self.df_subj[self.df_subj["ID"] == s]["processed_path"])[:-2]
+                str_path = str(self.df_subj[self.df_subj["ID"] == s]["processed_path"])
+                s_path = os.path.dirname()[:-2]
                 print(s_path)
 
                 for path, subdirs, files in os.walk(s_path):
