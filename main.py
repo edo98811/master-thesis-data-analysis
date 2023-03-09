bdf65c2b9fde394bb90f8e6fccbe57b6ec7481ef
edo98811 <edoardofilippi77@gmail.com>
2023-03-09 11:26:29 +0000

    debugging

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ DATA_FOLDER = "test_data/"
 
 
 def main():
-    ft.LogWriter.log_list.append("funziona?")
+    ft.LogWriter.clearlog()
     np.warnings.filterwarnings('error', category=np.VisibleDeprecationWarning)
 
     table = ft.Table("OASIS TABLE", BASE_PATH, OASIS_PATH,
